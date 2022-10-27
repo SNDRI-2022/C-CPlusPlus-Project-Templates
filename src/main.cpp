cc7a7ff4ca610a2c08f9091ed2d84a89946e6796
liu2830 <2305195328@qq.com>
2022-10-27 18:40:25 +0800

    Changes to be committed: 	modified:   .vscode/launch.json 	renamed:    include/test.hpp -> include/.gitkeep 	renamed:    lib/test -> lib/.geeikeep 	new file:   output/.gitkeep 	modified:   src/main.cpp
    Changes not staged for commit:
    	deleted:    output/main.exe
    	deleted:    src/main.o

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+/********************************************************************************
+ * @file main.cpp
+ * @author lxy (2305195328@qq.com)
+ * @brief
+ * @version 1.0.0
+ * @date 2022-10-27
+ ********************************************************************************/
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	std::cout << "TEST" << std::endl;
+	return 0;
 }
